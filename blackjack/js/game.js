ecbbc462f687b8b232c0e4c037e6a8bb8a17c4ec
Ruyi Li <ruyili2002@gmail.com>
2019-07-31 20:24:18 -0400

    Add blackjack

---
+++ blackjack/js/game.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', init);
+
+const $ = document.querySelector.bind(document);
+let hitBtn, foldBtn, playerHand, computerHand, status;
+
+function init () {
+    hitBtn = $('button#hit');
+    foldBtn = $('button#fold');
+    playerHand = $('p#playerHand');
+    computerHand = $('p#computerHand');
+    status = $('p#status');
+
+    hitBtn.addEventListener('click', hit);
+    foldBtn.addEventListener('click', fold);
+}
+
+function showCards () {
+    if (status.innerText !== 'Ongoing') return;
+    playerHand.innerText = player.join(' ');
+    computerHand.innerText = computer.join(' ');
+}
